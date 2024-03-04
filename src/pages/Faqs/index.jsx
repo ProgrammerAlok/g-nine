e24ffe70dbd6e9f872be90e93b9d5153bc0a32d4
ProgrammerAlok <alokpaul5584@gmail.com>
2024-03-04 19:05:27 +0530

    FAQs modified done

--- src/pages/Faqs/index.jsx
+++ src/pages/Faqs/index.jsx
@@ -18,10 +18,12 @@ const Faqs = () => {
             className="w-full object-cover rotate-180"
           />
         </div>
-        <div className="flex flex-row justify-end w-full max-w-[1440px] mx-auto bg-gray-100 py-10 ">
+        <div className="faq-container flex flex-row justify-end w-full max-w-[1440px] mx-auto bg-gray-100 py-10 ">
           <div className="h-[45rem] w-full relative">
+
+            {/* form & heading */}
             <div className="flex flex-col items-center justify-center w-full h-max gap-[50px] left-0 bottom-0 right-0 top-0 m-auto ">
-              {/* headings */}
+              
               <div className="flex flex-col items-center justify-start w-[69%] gap-5 relative">
                 <div className="flex flex-row justify-center w-[22%]">
                   <div className="flex flex-row justify-between items-center w-full">
@@ -59,14 +61,16 @@ const Faqs = () => {
               </div>
 
             </div>
+
+            {/* image */}
             <div className="flex flex-col items-start justify-start w-full top-0 right-0 left-0 m-auto absolute">
               <div className="flex flex-col items-start justify-start w-full">
-                <div className="flex flex-col items-start justify-start w-1/4 mt-[-1px] md:hidden">
-                  <div className="h-[203px] w-[203px] ml-[35px] bg-gray-100" />
+                <div className="flex flex-col items-start justify-start w-1/4 md:hidden">
+                  <div className="w-[303px] bg-gray-100" />
                   <Img
-                    src="images/img_mask_group.png"
+                    src="images/faq-image-01.png"
                     alt="image"
-                    className="w-full mt-[-128px] object-cover"
+                    className="h-full mt-[-128px] object-cover scale-x-[-1] translate-y-[80%] -translate-x-[6rem] "
                   />
                 </div>
               </div>

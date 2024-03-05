d639c483eeeb11277d04bf275ae06b68eb207857
ProgrammerAlok <alokpaul5584@gmail.com>
2024-03-05 16:50:07 +0530

    Home page Testimonials responsive done

--- src/components/Home/Testimonials/index.jsx
+++ src/components/Home/Testimonials/index.jsx
@@ -2,10 +2,11 @@ import { Heading, Img, RatingBar, Text } from "components";
 
 const Testimonials = () => {
   return (
-    <div className="flex flex-row justify-between w-[90%] max-w-[1440px] mx-auto">
-      <div className="flex flex-col items-center justify-start w-[46%] mb-[30px] gap-10">
+    <div className="flex md:flex-col-reverse justify-between w-[90%] max-w-[1440px] mx-auto ">
+      {/* left */}
+      <div className="flex flex-col items-center justify-start w-[46%] md:w-full mb-[30px] gap-10">
         <div className="flex flex-col items-start justify-start w-full gap-2.5">
-          <div className="flex flex-row justify-start items-center w-[32%] gap-2.5">
+          <div className="flex flex-row justify-start items-center w-full gap-2.5">
             <Img
               src="images/img_svg.svg"
               alt="svg_twentyone"
@@ -18,18 +19,20 @@ const Testimonials = () => {
             >
               Testimonials
             </Text>
-            <div className="flex w-9 h-[29px]" />
+            <Img
+              src="images/img_svg.svg"
+              alt="svg_twentyone"
+              className="h-[39px] scale-x-[-1]"
+            />
           </div>
-          <Heading size="2xl" as="h1" className="!leading-[80px]">
-            What customer saying
-            <br />
-            About Services
+          <Heading size="2xl" as="h1" className="">
+            <span className=""> What customer saying About Services </span>
           </Heading>
         </div>
-        <div className="flex flex-row justify-start items-center w-[99%] gap-[30px]">
-          <div className="flex flex-row justify-start w-[91%]">
+        <div className="flex md:flex-col justify-start items-center w-full gap-7 md:gap-0">
+          <div className="flex flex-row justify-start w-[91%] md:w-full">
             <div className="flex flex-row justify-start w-full">
-              <div className="flex flex-row justify-center w-full p-10 bg-gray-100 rounded-[9px]">
+              <div className="flex flex-row justify-center w-full p-10 md:p-2 bg-gray-100 rounded-[9px]">
                 <div className="flex flex-row justify-center w-full">
                   <div className="flex flex-col items-start justify-start w-full gap-3">
                     <Img
@@ -49,7 +52,7 @@ const Testimonials = () => {
                       scrambled it to make a type specimen book.
                     </Text>
                     <div className="flex flex-row justify-between items-center w-full">
-                      <div className="flex flex-row justify-start items-center w-2/5 gap-[15px]">
+                      <div className="flex flex-row justify-start items-center w-2/5 md:w-full gap-[15px]">
                         <Img
                           src="images/img_ellipse_591.png"
                           alt="circleimage"
@@ -84,11 +87,13 @@ const Testimonials = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-center justify-start w-[5%] gap-2.5">
+
+          {/* 1 2 3 */}
+          <div className="flex flex-col items-center justify-start md:-my-[8rem] w-[5%] gap-2.5 md:-rotate-90">
             <div className="h-20 w-0.5 bg-gradient1" />
             <div className="flex flex-col w-full gap-2.5">
               <div className="flex flex-col items-center justify-start w-full">
-                <div className="h-[35px] w-[34px] relative">
+                <div className="h-[35px] w-[34px] relative md:rotate-90">
                   <Img
                     src="images/img_group_6340.svg"
                     alt="image"
@@ -97,14 +102,14 @@ const Testimonials = () => {
                   <Text
                     size="xs"
                     as="p"
-                    className="justify-center w-max left-0 bottom-0 right-0 top-0 m-auto !text-gray-700_01 !font-inter text-center absolute"
+                    className="justify-center w-max left-0 bottom-0 right-0 top-0 m-auto !text-gray-700_01 !font-inter text-center absolute pt-1 md:pt-2"
                   >
                     1
                   </Text>
                 </div>
               </div>
               <div className="flex flex-col items-center justify-start w-full">
-                <div className="h-[35px] w-[34px] relative">
+                <div className="h-[35px] w-[34px] relative  md:rotate-90">
                   <Img
                     src="images/img_group_6341.svg"
                     alt="image"
@@ -113,14 +118,14 @@ const Testimonials = () => {
                   <Text
                     size="xs"
                     as="p"
-                    className="justify-center w-max left-0 bottom-0 right-0 top-0 m-auto !text-white-A700 !font-inter text-center absolute"
+                    className="justify-center w-max left-0 bottom-0 right-0 top-0 m-auto !text-white-A700 !font-inter text-center absolute pt-1 md:pt-2"
                   >
                     2
                   </Text>
                 </div>
               </div>
               <div className="flex flex-col items-center justify-start w-full">
-                <div className="h-[35px] w-[34px] relative">
+                <div className="h-[35px] w-[34px] relative  md:rotate-90">
                   <Img
                     src="images/img_group_6340.svg"
                     alt="image"
@@ -129,7 +134,7 @@ const Testimonials = () => {
                   <Text
                     size="xs"
                     as="p"
-                    className="justify-center w-max left-0 bottom-0 right-0 top-0 m-auto !text-gray-700_01 !font-inter text-center absolute"
+                    className="justify-center w-max left-0 bottom-0 right-0 top-0 m-auto !text-gray-700_01 !font-inter text-center absolute pt-1 md:pt-2"
                   >
                     3
                   </Text>
@@ -140,33 +145,35 @@ const Testimonials = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-row justify-center w-[49%] mt-[30px]">
-        <div className="h-[617px] w-full relative">
-          <div className="flex flex-row justify-start items-start w-[93%] h-full left-0 bottom-0 top-0 m-auto absolute">
+
+      {/* right */}
+      <div className="flex flex-row justify-center w-[49%] md:w-[100%] mt-[30px]">
+        <div className="h-[617px] md:h-[20rem] w-full relative">
+          <div className="flex flex-row justify-start items-start w-full h-full left-0 bottom-0 top-0 m-auto absolute">
             <Img
               src="images/img_testi_card_shape_3_png.png"
               alt="testicard_one"
-              className="w-[42%] mt-[66px] z-[1] object-cover"
+              className="w-[40%] mt-[66px] md:mt-1 z-[1] object-cover absolute"
             />
-            <div className="flex flex-col items-center justify-start h-[617px] w-[617px] ml-[-212px] p-[38px] rounded-tl-[50%] rounded-tr-[308px] rounded-bl-[50%] rounded-br-[308px] border-red-A400 border-4 border-dashed">
-              <div className="h-[532px] w-[532px] opacity-0.3 rotate-[90deg] border-red-A400_75 border-4 border-dashed rounded-[50%]" />
+            <div className="flex w-[80%] my-auto aspect-square p-[38px] rounded-full border-red-A400 border-4 border-dashed mx-auto">
+              <div className="w-full aspect-square opacity-0.3 rotate-[90deg] border-red-A400_75 border-4 border-dashed rounded-full" />
             </div>
           </div>
           <Img
             src="images/img_testi_card_img_png.png"
             alt="testicardimg"
-            className="justify-center h-[453px] w-[62%] left-0 bottom-0 right-0 top-0 m-auto object-cover absolute rounded-[225px]"
+            className="justify-center w-[60%] inset-0 m-auto object-cover absolute rounded-full "
           />
-          <div className="h-[532px] w-1/2 right-0 bottom-0 top-0 m-auto absolute">
+          <div className=" w-1/2 right-0 bottom-0 top-0 m-auto absolute">
             <Img
               src="images/img_testi_card_shape_1_png.png"
               alt="testicard_three"
-              className="h-[436px] w-full top-0 right-0 left-0 m-auto object-cover absolute"
+              className="w-[70%] inset-0 m-auto object-cover absolute"
             />
             <Img
               src="images/img_testi_card_shape_2_png.png"
               alt="testicard_five"
-              className="h-[181px] w-[62%] bottom-0 left-0 m-auto object-cover absolute"
+              className="w-[62%] left-0 bottom-4  m-auto object-cover absolute"
             />
           </div>
         </div>

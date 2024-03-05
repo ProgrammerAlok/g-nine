6eee6d617780110c2b38807db3e5ebe64340d2b8
ProgrammerAlok <alokpaul5584@gmail.com>
2024-03-05 13:01:16 +0530

    Room page responsive done

--- src/components/Rooms/RoomGallery/index.jsx
+++ src/components/Rooms/RoomGallery/index.jsx
@@ -4,8 +4,8 @@ const RoomGallery = () => {
     return (
       <div className="flex flex-col items-center max-w-[1440px] w-[90%] mx-auto justify-start gap-10 my-[5rem]">
         <Heading as="h3">Room Gallery</Heading>
-        <div className="flex flex-row justify-start items-start w-full gap-[30px]">
-          <div className="flex flex-col items-center justify-start w-[49%] gap-[30px]">
+        <div className="flex flex-row justify-start items-start w-full gap-7 md:gap-2 ">
+          <div className="flex flex-col items-center justify-start w-[49%] gap-7 md:gap-2 ">
             <Img
               src="images/img_rectangle_4371.png"
               alt="image"
@@ -27,7 +27,7 @@ const RoomGallery = () => {
               className="w-full object-cover rounded-[20px]"
             />
           </div>
-          <div className="flex flex-col items-center justify-start w-[49%] mt-[148px] gap-[30px]">
+          <div className="flex flex-col items-center justify-start w-[49%] mt-[148px] gap-7 md:gap-2 ">
             <Img
               src="images/img_rectangle_4372.png"
               alt="image_four"
